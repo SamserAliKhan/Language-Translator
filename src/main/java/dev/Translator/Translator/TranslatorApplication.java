9e64f41c2f7abf93ad945bed0c3fba8cd263f42e
SamserAliKhan <samseralikhan0448@gmail.com>
2023-09-27 04:18:32 +0530

    new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/dev/Translator/Translator/TranslatorApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/dev/Translator/Translator/TranslatorApplicationTests.java

---
+++ src/main/java/dev/Translator/Translator/TranslatorApplication.java
@@ -0,0 +1,17 @@
+package dev.Translator.Translator;
+
+import org.springframework.boot.SpringApplication;//cointains the run method 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslatorApplication.class, args);
+
+	}
+
+
+
+}
